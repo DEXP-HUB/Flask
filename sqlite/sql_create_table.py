9aa9381aa78b90393727af5dcf86bf33b2d804f3
Ali <ali_guseynov_02@mail.ru>
2024-09-12 18:38:14 +0300

    Add sql

---
+++ sqlite/sql_create_table.py
@@ -0,0 +1,20 @@
+import sqlite3
+create_table = """
+CREATE TABLE IF NOT EXISTS quotes (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+author TEXT NOT NULL,
+text TEXT NOT NULL
+);
+"""
+
+connection = sqlite3.connect("store.db")
+
+cursor = connection.cursor()
+
+cursor.execute(create_table)
+
+connection.commit()
+
+cursor.close()
+
+connection.close()
