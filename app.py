9d2d25048138e23a22b600e76afbb5fde25233d4
Ali <ali_guseynov_02@mail.ru>
2024-09-24 15:21:00 +0300

    CRUD

--- app.py
+++ app.py
@@ -3,46 +3,53 @@ import sqlite3
 
 app = Flask(__name__)
 
-# quotes = [
-# {
-# "id": 3,
-# "author": "Rick Cook",
-# "text": "Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает."
-# },
-# {
-# "id": 5,
-# "author": "Waldi Ravens",
-# "text": "Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках."
-# },
-# {
-# "id": 6,
-# "author": "Mosher’s Law of Software Engineering",
-# "text": "Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили."
-# },
-# {
-# "id": 8,
-# "author": "Yoggi Berra",
-# "text": "В теории, теория и практика неразделимы. На практике это не так."
-# },
-# ]
 
-@app.route("/get", methods=['GET'])
-def get_methods():
-   connection = sqlite3.connect("store.db")
-   cursor = connection.cursor()
-   get = cursor.execute("SELECT * FROM quotes").fetchall
-   return get
-   
-
-@app.route("/quotes")
+@app.route("/getquotes/", methods=['GET'])
 def get_quotes():
-   return quotes
+    connection = sqlite3.connect('sqlite/store.db')
+    quotes = connection.execute("""SELECT * FROM quotes""").fetchall()
+    connection.close()
+    return jsonify(quotes)
 
-@app.route("/quotes/<id>", methods=['POST'])
-def edit_quote(id):
-   quotes.append(id)
-   print(quotes)
 
-   
+@app.route("/quote/<int:id>", methods=["GET"])
+def get_quote(id):
+    connection = sqlite3.connect('sqlite/store.db')
+    quote = connection.execute("""SELECT * FROM quotes WHERE id = ?""", (id,)).fetchone()
+    connection.close()
+    return jsonify(id=quote[0], author=quote[1], quote=quote[2])
+
+
+@app.route("/addquotes/", methods=['POST'])
+def add_quote():
+    date = request.json
+    connection = sqlite3.connect('sqlite/store.db')
+    connection.execute("""INSERT INTO quotes (author, text) VALUES (?, ?)""",
+                       (date['author'], date['text']))
+    connection.commit()
+    connection.close()
+    return jsonify(status=200)
+
+
+@app.route("/updatequote/<int:id>", methods=['PUT'])
+def update_quote(id):
+    date = request.json
+    connection = sqlite3.connect('sqlite/store.db')
+    connection.execute("""UPDATE quotes SET author = ?, text = ? WHERE id = ?""",
+                       (date['author'], date['text'], id))
+    connection.commit()
+    connection.close()
+    return jsonify(status=200)
+
+
+@app.route("/deletequote/<int:id>", methods=['DELETE'])
+def delete_quote(id):
+    connection = sqlite3.connect('sqlite/store.db')
+    connection.execute("""DELETE FROM quotes WHERE id = ?""", (id,))
+    connection.commit()
+    connection.close()
+    return jsonify(status=200)
+
+
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=True)
